a21e5ec01c8cc18410b02bd8210a7233baf39775
Mariela <marielamiteva97@abv.bg>
2023-03-10 19:56:55 +0200

    Show games in catalog page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Routes, Route} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import * as gameService from './services/gameService';
+
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -8,6 +11,14 @@ import Catalog from './components/Catalog/Catalog';
 import './App.css';
 
 function App() {
+  const [games, setGames] = useState([]);
+    useEffect(() => {
+        gameService.getAll()
+        .then(result => {
+            setGames(result);
+        });
+
+    }, []);
   return (
 
     <div id="box">
@@ -15,11 +26,11 @@ function App() {
       {/* Main Content */}
       <main id="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home games={games} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={<CreateGame />} />
-          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog" element={<Catalog games={games} />} />
           
         </Routes>
       
